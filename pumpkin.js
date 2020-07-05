7c51328a2a889ada0d8b069cf89402431e518ae3
Kwon <sanghyukkwon90@gmail.com>
2020-07-05 09:07:18 +0900

    almost

--- pumpkin.js
+++ pumpkin.js
@@ -1,6 +1,34 @@
 const db = require('./dbConfig');
 const Sequelize = require('sequelize');
 
+const Pumpkin = db.define('Pumpkin', {
+    name: {
+        type: Sequelize.STRING
+    },
+    size: {
+        type: Sequelize.ENUM,
+        values: ['small','medium','large']
+    },
+    evil: {
+        type:Sequelize.BOOLEAN,
+        defaultValue: Math.random() >= 0.5
+        // Pumpkin.prototype.name += " GOOD" : Pumpkin.prototype.name += "EVIL"
+    },
+    carved: {
+        type: Sequelize.BOOLEAN,
+        defaultValue:false
+    },
+    candle: {
+        type: Sequelize.BOOLEAN,
+        defaultValue:false
+    },
+})
+
+Pumpkin.prototype.lightcandle = function() {
+    Pumpkin.candle = true
+    return Pumpkin.candle
+}
+
 //YOUR CODE GOES HERE
 
 //--------------------
